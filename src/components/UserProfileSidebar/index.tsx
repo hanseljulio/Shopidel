708b3499d8b60a818e0a293ca6408bc7956a09cb
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-10-31 14:21:42 +0700

    feat(user profile sidebar): layout complete

---
+++ src/components/UserProfileSidebar/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+import { FaUser, FaTag, FaDollarSign } from "react-icons/fa";
+
+function UserProfileSidebar() {
+  return (
+    <div className="user-profile-sidebar-div ">
+      <div className="admin-nav-wrapper fixed bg-[#59AFFF] w-[400px] h-screen">
+        <div className="username-section flex items-center gap-6 pt-[100px] ml-[40px]">
+          <Image
+            src={`/images/defaultuser.png`}
+            alt="Nothing"
+            width={50}
+            height={50}
+            className={`w-[50px] h-[50px]`}
+            style={{
+              objectFit: "cover",
+              borderRadius: "100%",
+            }}
+          />
+          <h1 className="text-white text-[30px]">Username</h1>
+        </div>
+        <div className="options-section text-white pt-[50px] ml-[50px]">
+          <h1 className="text-[25px] flex items-center gap-4">
+            <FaUser />
+            My Accounts
+          </h1>
+          <ul className="text-[18px] ml-[70px]">
+            <li className="py-1">Profile</li>
+            <li className="py-1">Addresses</li>
+            <li className="py-1">Change Password</li>
+          </ul>
+        </div>
+        <div className="remaining-section text-white ml-[50px] pt-[50px]">
+          <h1 className="text-[25px] flex items-center gap-4">
+            <FaDollarSign />
+            Purchase History
+          </h1>
+          <h1 className="text-[25px] flex items-center gap-4 pt-6">
+            <FaTag />
+            My Vouchers
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserProfileSidebar;
---
+++ src/pages/user/profile/index.tsx
@@ -0,0 +1,12 @@
+import UserProfileSidebar from "@/components/UserProfileSidebar";
+import React from "react";
+
+function UserProfile() {
+  return (
+    <div>
+      <UserProfileSidebar />
+    </div>
+  );
+}
+
+export default UserProfile;
