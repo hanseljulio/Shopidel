b981cfe61a2fa7a06e0e671ff2fb6edda0ff044a
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-10-31 15:51:42 +0700

    feat(user edit profile): responsive layout complete, working on sidebar navigation for mobile

--- src/pages/user/profile/index.tsx
+++ src/pages/user/profile/index.tsx
@@ -25,40 +25,40 @@ function UserProfile() {
         <UserProfileSidebar />
       </div>
       <div className="user-edit-profile-div ml-[680px] pt-[120px] space-y-5 mobile:mx-auto">
-        <div className="edit-profile-header pb-3">
+        <div className="edit-profile-header pb-3 mobile:text-center">
           <h1 className="text-[30px]">My Profile</h1>
           <p className="text-[18px]">Manage your account</p>
         </div>
 
         <form action="">
-          <div className="form-section-wrapper flex gap-[150px] mobile:flex-col">
+          <div className="form-section-wrapper flex gap-[150px] mobile:gap-[50px] mobile:flex-col mobile:items-center">
             <div className="form-section-div">
               <Input
                 label="Username"
                 labelStyle="mt-2"
-                styling="flex items-center gap-[57px] pb-[30px]"
+                styling="flex items-center gap-[57px] pb-[30px] mobile:flex-col mobile:gap-2 mobile:items-start"
                 width="w-[250px]"
                 type="text"
                 name="username"
               />
-              <div className="email-section flex items-center pb-[30px]">
-                <p>Email</p>
+              <div className="email-section flex items-center pb-[30px] mobile:flex-col mobile:items-start">
+                <p className="">Email</p>
                 {showEditEmail ? (
                   <Input
                     label=""
-                    styling="flex items-center gap-[91px]"
+                    styling="flex items-center gap-[91px] mobile:gap-0 mobile:py-4"
                     width="w-[250px]"
                     type="email"
                     name="emailEdit"
                   />
                 ) : (
-                  <p className="ml-[91px]">
+                  <p className="ml-[91px] mobile:m-0 mobile:py-4">
                     {emailConverter("hanseljulio@yahoo.com")}
                   </p>
                 )}
                 <p
                   onClick={toggleEditEmail}
-                  className="text-blue-600 underline hover:cursor-pointer ml-[30px]"
+                  className="text-blue-600 underline hover:cursor-pointer ml-[30px] mobile:m-0"
                 >
                   {showEditEmail ? "Go back" : "Change email"}
                 </p>
@@ -66,7 +66,7 @@ function UserProfile() {
               <Input
                 label="Phone"
                 labelStyle="mt-2"
-                styling="flex items-center gap-[85px] pb-[30px]"
+                styling="flex items-center gap-[85px] pb-[30px] mobile:flex-col mobile:gap-2 mobile:items-start"
                 width="w-[250px]"
                 type="text"
                 name="phoneNumber"
@@ -74,7 +74,7 @@ function UserProfile() {
               <Input
                 label="Shop Name"
                 labelStyle="mt-2"
-                styling="flex items-center gap-12 pb-[30px]"
+                styling="flex items-center gap-12 pb-[30px] mobile:flex-col mobile:gap-2 mobile:items-start"
                 width="w-[250px]"
                 type="text"
                 name="shopName"
@@ -82,7 +82,7 @@ function UserProfile() {
               <Input
                 label="Gender"
                 labelStyle="mt-2"
-                styling="flex items-center gap-[77px] pb-[30px]"
+                styling="flex items-center gap-[77px] pb-[30px] mobile:flex-col mobile:gap-2 mobile:items-start"
                 width="w-[250px]"
                 type="text"
                 name="username"
@@ -92,7 +92,7 @@ function UserProfile() {
                 type="date"
                 name="dateOfBirth"
                 value={""}
-                styling="flex items-center gap-[36px] pb-[30px]"
+                styling="flex items-center gap-[36px] pb-[30px] mobile:flex-col mobile:gap-2 mobile:items-start"
                 width="w-[250px]"
               />
             </div>
@@ -118,7 +118,7 @@ function UserProfile() {
               </label>
             </div>
           </div>
-          <div className="submit-btn">
+          <div className="submit-btn mobile:text-center mobile:py-[50px]">
             <h1>SUBMIT</h1>
           </div>
         </form>
