9e502b3955e15f9c1cc8f3a752b57b1346f2493c
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-10-31 18:38:50 +0700

    feat(PR-7): Configuring register page view and form

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import RegisterUserFirst from '../components/RegisterUserFirst'
+import RegisterUserSecond from '../components/RegisterUserSecond'
 
 const Register = () => {
+    const [page, setPage] = useState<JSX.Element>(
+        <RegisterUserFirst onNext={() => setPage(<RegisterUserSecond onSubmit={(data) => {
+            console.log(data)
+        }} />)} />
+    )
+
     return (
-        <div>Register</div>
+        <div className=' h-screen flex justify-between items-center'>
+            <div className='max-w-7xl w-full mx-auto flex justify-around items-center'>
+                <div>
+                    <Image src={"/auth_hero.png"} width={400} height={400} alt='auth_hero' />
+                </div>
+                <div className=''>
+                    {page}
+                </div>
+            </div>
+        </div>
     )
 }
 
