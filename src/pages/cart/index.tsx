1a6ada421ccd5c016d4e065e6ff76075792101d7
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-11-01 17:11:52 +0700

    styling(PR-43): added padding for the sidebar

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,26 +1,19 @@
 import Navbar from "@/components/Navbar";
 import React from "react";
 import Image from "next/image";
+import Button from "@/components/Button";
+import { useRouter } from "next/router";
+import EmptyCart from "@/components/EmptyCart";
 
 const CartPage = () => {
   return (
     <div>
       <Navbar />
-      <div className="cart-content-div lg:max-w-7xl mx-[351px]">
+      <div className="cart-content-div lg:max-w-7xl mx-[351px] mobile:mx-auto">
         <div className="titles-section mt-[30px]">
-          <h1 className="text-[30px]">My Cart</h1>
+          <h1 className="text-[30px] mobile:text-center">My Cart</h1>
         </div>
-        <div className="empty-card-div flex items-center justify-center">
-          <Image
-            alt="cart pic"
-            src={"/images/emptycart.png"}
-            width={250}
-            height={250}
-            objectFit="cover"
-            className="w-[250px] h-[250px]"
-          />
-        </div>
-        <h1 className="text-center">Your cart looks empty!</h1>
+        <EmptyCart />
       </div>
     </div>
   );
