23c5ae648b65f2793da75fd796f18a7603086b56
vira <amanda.virani95@gmail.com>
2023-11-02 15:52:57 +0700

    feat(PR-19): add spesification and description product

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -1,16 +1,18 @@
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import React, { useState } from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { BsStarFill } from "react-icons/bs";
-import { FaLocationDot, FaTruckFast } from "react-icons/fa6";
+import { FaCartShopping, FaLocationDot, FaTruckFast } from "react-icons/fa6";
 
 const ProductDetail = () => {
   const [count, setCount] = useState<number>(0);
   const [variationType, setVariationType] = useState<[]>([]);
   const [variation, setVariation] = useState<[]>([]);
+  let stock = 10;
 
   const inc = () => {
-    if (count >= 0) {
+    if (count >= 0 && count <= stock - 1) {
       setCount(count + 1);
     }
   };
@@ -23,14 +25,14 @@ const ProductDetail = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex flex-col md:flex-row justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 gap-4  py-5 ">
-        <div className="col imageProduct w-[1050px]">
+      <div className="grid-flow-col auto-cols-max justify-between px-4 mx-auto lg:max-w-7xl  md:flex md:px-8 gap-10 py-5">
+        <div className="col imageProduct w-full">
           <Image
             width={100}
             height={100}
             src="https://images.unsplash.com/photo-1697482036303-4c0cf56a38c3?auto=format&fit=crop&q=80&w=1973&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
-            className="bigImage w-[450px]"
+            className="bigImage w-full"
           />
           <div className="variation">
             <Image width={100} height={100} src="" alt="" />
@@ -61,6 +63,23 @@ const ProductDetail = () => {
             </div>
           </div>
 
+          {variationType !== null ? (
+            <div>
+              {variationType.map((n) => (
+                <div key={n}>
+                  <p>{"Variation Type (color/size)"}</p>
+                  {variation.map((n) => (
+                    <button key={n} type="submit">
+                      {"varian"}
+                    </button>
+                  ))}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <></>
+          )}
+
           <div className="flex text-center items-center">
             <div className="quantity flex">
               <button className="minus w-5" onClick={dec}>
@@ -69,6 +88,7 @@ const ProductDetail = () => {
               <input
                 className="inputQuantity text-center"
                 min={0}
+                max={100}
                 type="number"
                 value={count}
                 onChange={(e: any) => {
@@ -82,31 +102,52 @@ const ProductDetail = () => {
             <div className="stock text-xs text-neutral-500 py-3">
               <p>stock</p>
             </div>
-            {variationType !== null ? (
-              <div>
-                {variationType.map((n) => (
-                  <div key={n}>
-                    <p>{"Variation Type (color/size)"}</p>
-                    {variation.map((n) => (
-                      <button key={n} type="submit">
-                        {"varian"}
-                      </button>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            ) : (
-              <></>
-            )}
+          </div>
+          <div className="btn flex gap-5 mt-10">
+            <button
+              type="submit"
+              className="flex items-center justify-center border border-[#364968] hover:shadow-md bg-[#d6e4f8] p-2 w-36 hover:bg-[#eff6fd]  transition-all duration-300"
+            >
+              <AiOutlineShoppingCart /> <span>Add to cart</span>
+            </button>
+            <button
+              type="submit"
+              className=" bg-[#364968] text-white p-2 w-36 justify-center hover:bg-[#394e6f] hover:shadow-lg"
+            >
+              Buy now
+            </button>
           </div>
         </div>
-        <div className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ab
-          pariatur suscipit, porro rerum vero veniam nihil harum dolorum debitis
-          adipisci aperiam perspiciatis quam mollitia et similique unde
-          doloremque labore delectus enim? Quis dignissimos asperiores
-          voluptatum quaerat quod, vel assumenda optio quibusdam illum! Ex quae
-          rem aliquam debitis incidunt eligendi.
+        <div className="description mt-10 md:mt-0 ">
+          <div className="spesification">
+            <p className="text-lg font-medium border-b">
+              Product Specifications
+            </p>
+            <table>
+              <thead></thead>
+              <tbody>
+                <tr>
+                  <td>{"Brand"}</td>
+                  <td>lorem</td>
+                </tr>
+                <tr>
+                  <td>{"Stock"}</td>
+                  <td>ipsum</td>
+                </tr>
+                <tr>
+                  <td>{"Shipped from"}</td>
+                  <td>Malang</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="desc pt-10">
+            <p className="text-lg font-medium border-b">Description</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ab
+              pariatur suscipit, porro rerum vero ven
+            </p>
+          </div>
         </div>
       </div>
       <div className=""></div>
