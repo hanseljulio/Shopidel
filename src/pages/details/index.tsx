a946794a6902f664dfdd28b42845f405e7f37750
vira <amanda.virani95@gmail.com>
2023-11-03 11:29:14 +0700

    style(PR-19): change the layout of the product detail page

--- src/pages/details/index.tsx
+++ src/pages/details/index.tsx
@@ -28,162 +28,7 @@ const ProductDetail = () => {
       <Navbar />
       <div className="mx-auto lg:max-w-7xl px-4 md:px-0">
         <div className="flex-col md:flex-row justify-between md:flex gap-10 py-5 px-5">
-          <div className="order-1 md:order-1 imageProduct w-full md:w-1/3">
-            <div
-              id="indicators-carousel"
-              className="relative"
-              data-carousel="static"
-            >
-              <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-                <div
-                  className="hidden duration-700 ease-in-out"
-                  data-carousel-item="active"
-                >
-                  <Image
-                    width={10}
-                    height={10}
-                    src="/images/auth_hero.png"
-                    className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                    alt="..."
-                  />
-                </div>
-                <div
-                  className="hidden duration-700 ease-in-out"
-                  data-carousel-item
-                >
-                  <Image
-                    width={10}
-                    height={10}
-                    src="/docs/images/carousel/carousel-2.svg"
-                    className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                    alt="..."
-                  />
-                </div>
-                <div
-                  className="hidden duration-700 ease-in-out"
-                  data-carousel-item
-                >
-                  <Image
-                    width={10}
-                    height={10}
-                    src="/docs/images/carousel/carousel-3.svg"
-                    className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                    alt="..."
-                  />
-                </div>
-                <div
-                  className="hidden duration-700 ease-in-out"
-                  data-carousel-item
-                >
-                  <Image
-                    width={10}
-                    height={10}
-                    src="/docs/images/carousel/carousel-4.svg"
-                    className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                    alt="..."
-                  />
-                </div>
-                <div
-                  className="hidden duration-700 ease-in-out"
-                  data-carousel-item
-                >
-                  <Image
-                    width={10}
-                    height={10}
-                    src="/docs/images/carousel/carousel-5.svg"
-                    className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                    alt="..."
-                  />
-                </div>
-              </div>
-              {/* <!-- Slider indicators --> */}
-              <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-                <button
-                  type="button"
-                  className="w-3 h-3 rounded-full"
-                  aria-current="true"
-                  aria-label="Slide 1"
-                  data-carousel-slide-to="0"
-                ></button>
-                <button
-                  type="button"
-                  className="w-3 h-3 rounded-full"
-                  aria-current="false"
-                  aria-label="Slide 2"
-                  data-carousel-slide-to="1"
-                ></button>
-                <button
-                  type="button"
-                  className="w-3 h-3 rounded-full"
-                  aria-current="false"
-                  aria-label="Slide 3"
-                  data-carousel-slide-to="2"
-                ></button>
-                <button
-                  type="button"
-                  className="w-3 h-3 rounded-full"
-                  aria-current="false"
-                  aria-label="Slide 4"
-                  data-carousel-slide-to="3"
-                ></button>
-                <button
-                  type="button"
-                  className="w-3 h-3 rounded-full"
-                  aria-current="false"
-                  aria-label="Slide 5"
-                  data-carousel-slide-to="4"
-                ></button>
-              </div>
-              {/* <!-- Slider controls --> */}
-              <button
-                type="button"
-                className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                data-carousel-prev
-              >
-                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg
-                    className="w-4 h-4 text-white dark:text-gray-800"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M5 1 1 5l4 4"
-                    />
-                  </svg>
-                  <span className="sr-only">Previous</span>
-                </span>
-              </button>
-              <button
-                type="button"
-                className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                data-carousel-next
-              >
-                <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                  <svg
-                    className="w-4 h-4 text-white dark:text-gray-800"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 6 10"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m1 9 4-4-4-4"
-                    />
-                  </svg>
-                  <span className="sr-only">Next</span>
-                </span>
-              </button>
-            </div>
+          <div className="order-1 md:order-1 imageProduct w-full md:w-1/4">
             {/* <Image
               width={100}
               height={100}
@@ -191,16 +36,8 @@ const ProductDetail = () => {
               alt=""
               className="bigImage w-full"
             /> */}
-            <div className="variation">
-              <Image
-                width={100}
-                height={100}
-                src="/images/auth_hero.png"
-                alt=""
-              />
-            </div>
           </div>
-          <div className="order-2 md:order-3 purchaseBox border shadow-inner rounded-sm p-5 h-fit md:w-1/3">
+          <div className="order-2 md:order-3 purchaseBox border shadow-inner rounded-sm p-5 h-fit md:w-1/4 md:sticky">
             <p className="productTitle text-lg font-medium pb-3">
               Lorem ipsum dolor sit amet
             </p>
@@ -281,7 +118,7 @@ const ProductDetail = () => {
             </div>
           </div>
 
-          <div className="order-3 md:order-2 description mt-10 md:mt-0 md:w-1/3">
+          <div className="order-3 md:order-2 description mt-10 md:mt-0 md:w-2/4">
             <div className="spesification">
               <p className="text-lg font-medium border-b my-4">
                 Product Specifications
@@ -311,86 +148,118 @@ const ProductDetail = () => {
                 pariatur suscipit, porro rerum vero ven. Lorem ipsum, dolor sit
                 amet consectetur adipisicing elit. Deleniti explicabo
                 dignissimos veritatis sapiente quibusdam possimus labore unde?
-                Fugit, vitae reprehenderit?
+                Fugit, vitae reprehenderit? Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Dolorum nesciunt voluptates
+                obcaecati quaerat aspernatur molestias necessitatibus
+                praesentium animi maxime magnam quasi vitae hic exercitationem
+                error, sequi eos ullam illo repellat sunt debitis laudantium
+                quibusdam. Quod aperiam sed nam fugit corporis maxime sunt
+                architecto aliquam distinctio quis exercitationem reprehenderit
+                soluta dolores suscipit praesentium, voluptate ad molestiae
+                placeat quam. Quod incidunt corporis consequatur neque hic
+                harum, nam sequi tempore, nostrum at aperiam a magni ab
+                perspiciatis impedit nulla modi error ut animi odio aliquid
+                porro. Fugiat error sapiente dicta doloremque quia fugit earum
+                ducimus nam tenetur similique ipsum, harum dolorem ipsam enim?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Deleniti beatae officiis at esse placeat quis laboriosam,
+                repellat sed iure et saepe unde atque ex odio labore tempora
+                molestias, quasi quas quos deserunt quidem ipsam totam? Animi
+                neque deserunt nostrum quod architecto! Asperiores cum facere
+                consequatur rerum voluptatum quae dicta. Sit delectus, eius
+                possimus repellendus maiores veritatis, consequatur voluptates
+                fugit consectetur doloribus quis repudiandae ratione impedit
+                alias? Repellat quibusdam at quis consequatur sed blanditiis
+                aliquam reprehenderit laudantium. Quia ab molestiae, quibusdam,
+                dolores quisquam, nulla neque maxime alias harum nemo eaque
+                omnis! Obcaecati ipsa esse itaque consequatur molestiae, numquam
+                excepturi. Magnam exercitationem iusto accusantium voluptatum
+                quaerat id saepe, debitis maxime, reiciendis obcaecati unde
+                excepturi numquam est, inventore autem deleniti suscipit dolores
+                natus odit. Dolores dicta adipisci veniam illum repellendus
+                quaerat magni distinctio alias magnam? Laborum iure est saepe
+                doloribus culpa numquam, eos asperiores repellendus eaque!
+                Earum, omnis sed. Neque natus quidem nulla. Magni porro tenetur
+                culpa sint explicabo ducimus quos magnam, eos eveniet dolorum
+                nam consectetur voluptatibus consequuntur animi inventore ut.
+                Rerum quae maxime accusantium est quas architecto excepturi
+                voluptates atque? Illum atque rem numquam, reiciendis
+                reprehenderit possimus cum blanditiis ad minima animi eos esse
+                repudiandae, commodi hic itaque mollitia expedita, in harum
+                fugit modi dolores! Officiis ut veniam id tenetur nemo
+                repudiandae deserunt labore ipsam. Eos accusantium adipisci
+                nobis sit necessitatibus nemo odio quos, consectetur voluptatum
+                libero mollitia nisi expedita animi molestiae pariatur quam
+                voluptatibus in. Voluptatibus natus reiciendis asperiores
+                quaerat odit! Nihil optio eveniet laboriosam, dolorum distinctio
+                deserunt dignissimos. Possimus, eligendi provident. Sunt quam
+                repellendus corporis porro dolorum expedita voluptates, officiis
+                accusantium odio! Amet earum quam, quisquam odio distinctio
+                mollitia, quis nobis vel eligendi debitis facere vero recusandae
+                accusamus non provident deserunt, adipisci optio soluta libero
+                illo aspernatur beatae. Animi id eveniet possimus quis
+                consequatur, laudantium reprehenderit rerum incidunt aliquam.
               </p>
             </div>
-          </div>
-        </div>
-        <div className="sellerShop bg-[#364968] flex flex-row gap-y-5 text-white py-3 gap-10 px-5 ">
-          <Image
-            width={90}
-            height={0}
-            src={"/images/defaultuser.png"}
-            alt="seller"
-            className="imgSeller w-20 h-full"
-          />
-          <div className="aboutSeller flex flex-col md:flex-row justify-between w-1/2 md:w-full">
-            <div className="order-1">
-              <p>Nama Toko</p>
-              <p>
-                <button className="flex gap-2 items-center w-full border border-[#fddf97] hover:shadow-lg   p-1 md:w-36 text-[#fddf97] hover:bg-[#1c2637]  transition-all duration-300">
-                  <FaStore /> <p>Visit the store</p>
-                </button>
-              </p>
-            </div>
-            <div className="order-2">
-              <p className="flex justify-between md:gap-20">
-                Rating
-                <span className="flex items-center text-[#fddf97]">
-                  <FaStar />
-                  {"5"}
-                </span>
-              </p>
-              <p className="flex justify-between md:gap-20">
-                Product <span className="text-[#fddf97]">{"30"}</span>
-              </p>
-            </div>
-            <div className="order-3">
-              <p className="flex justify-between md:gap-20">
-                Year of joining <span className="text-[#fddf97]">{"2018"}</span>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="reviews">
-          <p className="reviewsProduct text-lg font-semibold">
-            Product Reviews
-          </p>
-          <div>
-            <div>
-              <p>{"4.8 dari 5"}</p>
-              <div className="star flex">
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
+            <div className="sellerShop bg-[#364968] flex flex-row gap-y-5 text-white py-3 my-10 gap-10 px-5 ">
+              <Image
+                width={90}
+                height={0}
+                src={"/images/defaultuser.png"}
+                alt="seller"
+                className="imgSeller w-20 h-full"
+              />
+              <div className="aboutSeller justify-between w-1/2 md:w-full">
+                <p>Nama Toko</p>
+                <p>
+                  <button className="flex gap-1 md:gap-2 mt-3 text-sm items-center w-full border border-[#fddf97] hover:shadow-lg   p-1 md:w-36 text-[#fddf97] hover:bg-[#1c2637]  transition-all duration-300">
+                    <FaStore /> <p>Visit the store</p>
+                  </button>
+                </p>
               </div>
             </div>
-            <div className="buyerReviews flex">
-              <div className="imageCust">
-                <Image
-                  width={100}
-                  height={100}
-                  src={"/images/auth_hero.png"}
-                  alt=".."
-                />
-              </div>
-              <div className="bodyReview flex-row gap-y-5">
-                <p className="custName">{"cust name"}</p>
-                <p className="flex">
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                  <FaStar />
-                </p>
-                <p className="dateReview">
-                  {"2023-06-05"}| {"Variasi: S"}
-                </p>
-                <p className="theReview">
-                  {" terlalu mahal dengan kwalitas kain yg nerawang"}
-                </p>
+            <div className="reviews">
+              <p className="reviewsProduct text-lg font-semibold">
+                Product Reviews
+              </p>
+              <div>
+                <div>
+                  <p>{"4.8 dari 5"}</p>
+                  <div className="star flex">
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                    <FaStar />
+                  </div>
+                </div>
+                <div className="buyerReviews flex mt-5">
+                  <div className="imageCust pr-4 rounded-full overflow-hidden">
+                    <Image
+                      width={100}
+                      height={100}
+                      src={"/images/auth_hero.png"}
+                      alt=".."
+                    />
+                  </div>
+                  <div className="bodyReview flex-row gap-y-5">
+                    <p className="custName">{"cust name"}</p>
+                    <p className="flex">
+                      <FaStar />
+                      <FaStar />
+                      <FaStar />
+                      <FaStar />
+                      <FaStar />
+                    </p>
+                    <p className="dateReview text-sm text-neutral-500">
+                      {"2023-06-05"}| {"Variasi: S"}
+                    </p>
+                    <p className="theReview">
+                      {/* {title.length > 20 ? `${title.substring(0, 23)}...` : title} */}
+                      {" terlalu mahal dengan kwalitas kain yg nerawang"}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
