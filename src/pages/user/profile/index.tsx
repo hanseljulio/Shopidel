3e4615e515b79e072d7fea1fcc188cc1eca76318
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-11-07 10:50:49 +0700

    feat(PR-43): replaced gender input with a dropdown

--- src/pages/user/profile/index.tsx
+++ src/pages/user/profile/index.tsx
@@ -15,6 +15,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { getCookie } from "cookies-next";
 import { emailConverter } from "@/utils/utils";
+import Dropdown from "@/components/Dropdown";
 
 const UserProfile = ({
   userData,
@@ -199,14 +200,13 @@ const UserProfile = ({
                     })
                   }
                 />
-                <Input
+                <Dropdown
                   label="Gender"
                   labelStyle="mt-2"
-                  styling="flex items-center gap-[77px] pb-[30px] mobile:flex-col mobile:gap-2 mobile:items-start"
                   width="w-[250px]"
-                  type="text"
-                  name="username"
                   value={userDetails!.gender}
+                  flexLabel="flex items-center gap-[77px] pb-[30px] mobile:flex-col mobile:gap-2 mobile:items-start"
+                  options={["male", "female"]}
                   onChange={(e) =>
                     setUserDetails({
                       ...userDetails!,
@@ -234,12 +234,13 @@ const UserProfile = ({
                 className={`flex-col justify-center items-center admin-edit-photo p-4 mobile:mx-auto`}
               >
                 <Image
-                  src={`${imageFile === null
-                    ? "/images/defaultuser.png"
-                    : imageFile === undefined
+                  src={`${
+                    imageFile === null
+                      ? "/images/defaultuser.png"
+                      : imageFile === undefined
                       ? "/images/defaultuser.png"
                       : URL.createObjectURL(imageFile)
-                    }`}
+                  }`}
                   alt="Nothing"
                   width={200}
                   height={200}
