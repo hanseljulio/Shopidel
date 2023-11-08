66cf98dca216184c737e71b2440e1c7034fe746a
jeppung <jeppung@gmail.com>
2023-11-09 00:24:24 +0700

    feat(PR-95): Removing checkAuth function

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,53 +11,15 @@ export const middleware = async (req: NextRequest) => {
   let accessToken = req.cookies.get("accessToken");
   let refreshToken = req.cookies.get("refreshToken");
 
-  const checkAuth = async () => {
-    if (accessToken === undefined) {
-      if (refreshToken !== undefined) {
-        try {
-          const res = await fetch(
-            "https://digitalent.games.test.shopee.io/vm2/api/accounts/refresh-token",
-            {
-              method: "POST",
-              body: JSON.stringify({
-                refresh_token: (refreshToken as RequestCookie).value,
-              }),
-            }
-          );
-
-          if (!res.ok) {
-            console.log("meh1");
-            throw new Error(res.statusText);
-          }
-          const data = ((await res.json()) as IAPIResponse<IAPILoginResponse>)
-            .data;
-
-          req.cookies.set("accessToken", data?.access_token!);
-          req.cookies.set("refreshToken", data?.refresh_token!);
-          return true;
-        } catch (e) {
-          console.log(e);
-          req.cookies.delete("refreshToken");
-          return false;
-        }
-      } else {
-        console.log("meh");
-        return false;
-      }
-    }
-    return true;
-  };
-
   if (req.nextUrl.pathname.startsWith("/user")) {
-    if (await checkAuth()) {
-      console.log("yes");
+    if (accessToken !== undefined) {
       return NextResponse.next();
     }
     return NextResponse.redirect(new URL("/", req.url));
   }
 
   if (req.nextUrl.pathname.startsWith("/login")) {
-    if (await checkAuth()) {
+    if (accessToken !== undefined) {
       return NextResponse.redirect(new URL("/", req.url));
     }
     return NextResponse.next();
