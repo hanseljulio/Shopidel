60d83902dc63f501330111aaf03281244cd662a4
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-11-09 15:46:12 +0700

    feat(PR-95): Removing /user middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export const middleware = (req: NextRequest) => {
   const accessToken = req.cookies.get("accessToken");
 
-  if (req.nextUrl.pathname.startsWith("/user")) {
-    if (accessToken !== undefined) {
-      return NextResponse.next();
-    }
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-
   if (req.nextUrl.pathname.startsWith("/login")) {
     if (accessToken !== undefined) {
       return NextResponse.redirect(new URL("/", req.url));
