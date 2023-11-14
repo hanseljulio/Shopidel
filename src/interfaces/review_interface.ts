855f877e13bb836e4c1a71c77c7f4b428fee5d3b
vira <amanda.virani95@gmail.com>
2023-11-14 11:22:48 +0700

    feat(PR-19): adding review interface

---
+++ src/interfaces/review_interface.ts
@@ -0,0 +1,9 @@
+export interface IReviewProduct
+{
+    customer_name: string,
+    customer_picture_url: string
+    stars: string,
+    comment: string,
+    variant: string,
+    created_at: string
+  }
