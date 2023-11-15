6eae48de76dde38a5a4f16234c43d757da012575
vira <amanda.virani95@gmail.com>
2023-11-15 18:04:05 +0700

    feat(PR-15): adding onerror image category

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -9,9 +9,14 @@ function Category(props: ICategoryProps) {
   return (
     <div>
       <img
-        src={`/categories/${props.src}`}
+        src={props.src}
         alt={props.alt}
         className="rounded-full w-14 h-14 md:w-20 md:h-20 object-cover flex mx-auto"
+        placeholder="https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"
+        onError={(e) => {
+          (e.target as HTMLInputElement).src =
+            "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png";
+        }}
       />
       <p className="text-center text-xs md:text-sm">{props.text}</p>
     </div>
