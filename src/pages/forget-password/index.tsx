9e3e02fce5a32a9b5fd6c3112fd0c6d13c2d53bb
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-11-15 19:45:01 +0700

    feat(PR-130): Adding new pages

---
+++ src/pages/forget-password/index.tsx
@@ -0,0 +1,75 @@
+import Button from "@/components/Button";
+import { IAPIResponse } from "@/interfaces/api_interface";
+import { IUser } from "@/interfaces/user_interface";
+import { API } from "@/network";
+import axios from "axios";
+import React, { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ForgetPassword = () => {
+  const { register, handleSubmit } = useForm<Pick<IUser, "email">>();
+  const [successMsg, setSuccessMsg] = useState<string>("");
+
+  const onSubmit: SubmitHandler<Pick<IUser, "email">> = async (data) => {
+    try {
+      const res = await toast.promise(
+        API.post("/auth/request-forget-password", data),
+        {
+          pending: "Loading",
+          error: {
+            render({ data }) {
+              if (axios.isAxiosError(data)) {
+                const res = data.response?.data as IAPIResponse;
+                return res.message;
+              }
+              console.log(data);
+            },
+          },
+        },
+        {
+          autoClose: 1500,
+          position: "top-center",
+        }
+      );
+
+      setSuccessMsg((res.data as IAPIResponse).message!);
+    } catch (e) {}
+  };
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <ToastContainer />
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-bold">Forget Password</h1>
+        {successMsg !== "" ? (
+          <h1>{successMsg}</h1>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)} className="mt-5">
+            <div className="flex flex-col">
+              <label htmlFor="email" className="text-sm">
+                Email
+              </label>
+              <input
+                {...register("email", { required: "Email is required" })}
+                type="email"
+                name="email"
+                id="email"
+                className="rounded-md border p-2"
+              />
+            </div>
+            <div className="mt-2">
+              <Button
+                text="Submit"
+                styling="bg-[#364968] text-white w-full rounded-md p-2"
+              />
+            </div>
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ForgetPassword;
---
+++ src/pages/reset-password/index.tsx
@@ -0,0 +1,99 @@
+import Button from "@/components/Button";
+import { IRegisterForm } from "@/interfaces/auth_interface";
+import * as crypto from "crypto";
+import { GetServerSidePropsContext } from "next";
+import { useRouter } from "next/router";
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { ToastContainer } from "react-toastify";
+
+const ResetPassword = () => {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<Pick<IRegisterForm, "password" | "confirmPassword">>();
+
+  // console.log(decrypt(router.query.id?.toString()!));
+  console.log(process.env);
+
+  const onSubmit: SubmitHandler<
+    Pick<IRegisterForm, "password" | "confirmPassword">
+  > = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <ToastContainer />
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-bold">Reset Password</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-5">
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-sm">
+              Password
+            </label>
+            <input
+              {...register("password", {
+                validate: {
+                  required: (v) => v.length !== 0 || "Password is required",
+                  minCharacters: (v) =>
+                    v.length >= 8 || "Password minimum 8 characters",
+                  maxCharacters: (v) =>
+                    v.length <= 20 || "Password maximum 20 characters",
+                  includeUppercase: (v) =>
+                    /[A-Z]/g.test(v) || "Must include uppercase character",
+                  notIncludeUsername: (v) =>
+                    !v.includes("") || "Cannot contain username",
+                },
+              })}
+              type="password"
+              name="password"
+              id="password"
+              className="rounded-md border p-2"
+            />
+            {errors.password?.type === "validate" && (
+              <p role="alert" className="text-xs text-red-500 mt-1">
+                {errors.password.message}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="email" className="text-sm">
+              Confirm Password
+            </label>
+            <input
+              {...register("confirmPassword", {
+                validate: {
+                  required: (v) =>
+                    v.length !== 0 || "Confirm password is required",
+                  matchPassword: (v) =>
+                    v === getValues("password") || "Password not match",
+                },
+              })}
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              className="rounded-md border p-2"
+            />
+            {errors.confirmPassword?.type === "validate" && (
+              <p role="alert" className="text-xs text-red-500 mt-1">
+                {errors.confirmPassword.message}
+              </p>
+            )}
+          </div>
+          <div className="mt-2">
+            <Button
+              text="Submit"
+              styling="bg-[#364968] text-white w-full rounded-md p-2"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ResetPassword;
