4b7f397fcb40f74390a19a514c9b9972f71dc800
Kevin Joshua Pulung <kevin.pulung@shopee.com>
2023-11-20 16:36:27 +0700

    feat(PR-146): Configure initial UI

--- src/pages/myshop/products/index.tsx
+++ src/pages/myshop/products/index.tsx
@@ -1,23 +1,122 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SellerAdminLayout from "@/components/SellerAdminLayout";
 import Button from "@/components/Button";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { API } from "@/network";
+import { IAPIPagination, IAPIResponse } from "@/interfaces/api_interface";
+import { IProduct } from "@/interfaces/product_interface";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import Pagination from "@/components/Pagination";
 
 const SellerAdminProducts = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const [pagination, setPagination] = useState<IAPIPagination>();
   const router = useRouter();
+
+  const getSellerProducts = async () => {
+    try {
+      const res = await API.get("/sellers/products", {
+        params: {
+          ...router.query,
+        },
+      });
+      const data = res.data as IAPIResponse<IProduct[]>;
+
+      setProducts(data.data!);
+      setPagination(data.pagination!);
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        if (e.response?.status === 403) {
+          return router.push("/");
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    getSellerProducts();
+  }, []);
 
   return (
     <SellerAdminLayout currentPage="Products">
-      <div className="w-full mx-auto mt-10">
-        <div className="flex items-center md:flex-row justify-between px-[50px] md:gap-0 flex-col gap-6">
+      <div className="p-5 flex flex-col">
+        <div className="flex items-center justify-between">
           <h1 className="text-[30px]">My Products</h1>
-
           <Button
             text="Add new product"
             onClick={() => router.push("/myshop/products/add-product")}
-            styling="bg-[#fddf97] p-3 rounded-[8px] w-[200px] my-4"
+            styling="bg-[#364968] text-white p-2 rounded-md text-sm px-3"
           />
         </div>
+        {products.length !== 0 ? (
+          <div className="flex flex-col">
+            <div className="mt-5 h-[500px]">
+              <table className="w-full border-collapse border">
+                <thead>
+                  <tr>
+                    <th className="text-start p-2 border-b">No</th>
+                    <th className="text-start p-2 border-b">Name</th>
+                    <th className="text-start p-2 border-b">Created At</th>
+                    <th className="text-start p-2 border-b">Updated At</th>
+                    <th className="text-start p-2 border-b">Action</th>
+                  </tr>
+                </thead>
+                <thead>
+                  {products.map((product, i) => {
+                    return (
+                      <tr
+                        key={i}
+                        className={`${(i + 1) % 2 == 0 && "bg-slate-100"}`}
+                      >
+                        <td className="p-2">{i + 1}</td>
+                        <td className="p-2">{product.name}</td>
+                        <td className="p-2">
+                          {new Date(product.created_at).toLocaleString()}
+                        </td>
+                        <td className="p-2">
+                          {new Date(product.updated_at).toLocaleString()}
+                        </td>
+                        <td className="p-2">
+                          <div className="flex justify-around w-full h-full">
+                            <AiFillEdit className="hover:cursor-pointer" />
+                            <AiFillDelete
+                              color={"red"}
+                              className="hover:cursor-pointer"
+                            />
+                          </div>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </thead>
+              </table>
+            </div>
+            <div className="self-end">
+              <Pagination
+                data={pagination}
+                onNavigate={(page) =>
+                  router.push({
+                    href: router.asPath,
+                    query: {
+                      page: page,
+                    },
+                  })
+                }
+              />
+            </div>
+          </div>
+        ) : (
+          <div className="mt-10">
+            <div
+              className="flex flex-col h-full
+             items-center gap-y-5"
+            >
+              <img className="w-80" src="/images/create-product.png" />
+              <h1 className="text-2xl font-bold">Create your first product!</h1>
+            </div>
+          </div>
+        )}
       </div>
     </SellerAdminLayout>
   );
