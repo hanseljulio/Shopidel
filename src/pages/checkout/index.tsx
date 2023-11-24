3d1cb04925baa38ce8c1524670c7257b2d67d1ff
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-11-24 16:16:11 +0700

    refactor(PR-152): moved some checkout components into the checkout file

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -4,7 +4,7 @@ import CheckoutTableHead from "@/components/CheckoutTableHead";
 import CheckoutTableData from "@/components/CheckoutTableData";
 import { clientUnauthorizeHandler, currencyConverter } from "@/utils/utils";
 import Button from "@/components/Button";
-import CheckoutVoucherSelect from "@/components/CheckoutVoucherSelect";
+import { FaTicketAlt } from "react-icons/fa";
 import CheckoutShippingSelect from "@/components/CheckoutShippingSelect";
 import Footer from "@/components/Footer";
 import CheckoutGrandTotal from "@/components/CheckoutGrandTotal";
@@ -14,7 +14,7 @@ import { FaMapMarkerAlt, FaTag } from "react-icons/fa";
 import Modal from "@/components/Modal";
 import CheckoutVoucherModal from "@/components/CheckoutVoucherModal";
 import CheckoutAddressModal from "@/components/CheckoutAddressModal";
-import CheckoutPayment from "@/components/CheckoutPayment";
+import { FaWallet } from "react-icons/fa";
 import { ICartData } from "@/interfaces/cart_interface";
 import { useCartStore } from "@/store/cartStore";
 import { API } from "@/network";
@@ -67,6 +67,64 @@ interface IShippingCostData {
 interface IEnterWalletPinModalProps {
   submitFunction: (pin: string) => void;
 }
+
+interface ICheckoutPaymentProps {
+  money: number;
+  canPay: boolean;
+}
+
+interface CheckoutVoucherSelectProps {
+  usedVoucher: number;
+  voucherName: string;
+  modalOn: () => void;
+}
+
+const CheckoutVoucherSelect = (props: CheckoutVoucherSelectProps) => {
+  return (
+    <div className="text-[20px] px-[20px] flex justify-between border-2 py-4 items-center md:flex-row flex-col gap-6">
+      <h1 className="flex items-center gap-3">
+        <FaTicketAlt className="text-[#e09664]" />
+        Promotions
+      </h1>
+      <span
+        className={`text-[#bb5d1f] ${
+          props.usedVoucher === 0 ? "hidden invisible" : ""
+        }`}
+      >
+        Used: {props.voucherName.substring(0, 25) + "..."}
+      </span>
+      <h1
+        onClick={props.modalOn}
+        className="text-[18px] text-blue-600 hover:cursor-pointer hover:underline"
+      >
+        Select Promotion
+      </h1>
+    </div>
+  );
+};
+
+const CheckoutPayment = (props: ICheckoutPaymentProps) => {
+  return (
+    <div className="text-[20px] px-[20px] flex justify-between border-2 py-4 items-center md:flex-row flex-col md:gap-0 gap-5">
+      <h1 className="flex items-center gap-3">
+        <FaWallet className=" text-amber-700" />
+        My Wallet
+      </h1>
+      <span
+        className={`text-red-600 ${props.canPay ? "hidden invisible" : ""}`}
+      >
+        INSUFFICIENT FUNDS
+      </span>
+      <h1
+        className={`text-[20px] ${
+          props.canPay ? "text-blue-600" : "text-red-600"
+        } font-bold`}
+      >
+        {currencyConverter(props.money)}
+      </h1>
+    </div>
+  );
+};
 
 const NoAddressModal = () => {
   const router = useRouter();
