ec0b2ede6f57017854ab51e725d6e0e73982de06
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-11-28 15:59:55 +0700

    refactor(PR-165): changed empty cart in cart hover to be reusable

--- src/components/EmptyCart/index.tsx
+++ src/components/EmptyCart/index.tsx
@@ -1,13 +1,9 @@
 import React from "react";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import Button from "../Button";
 
 const EmptyCart = () => {
-  const router = useRouter();
   return (
     <div>
-      <div className="empty-card-div flex items-center justify-center mt-[30px]">
+      <div className="empty-card-div flex items-center justify-center">
         <img
           alt="cart pic"
           src={"/images/emptycart.png"}
@@ -17,13 +13,6 @@ const EmptyCart = () => {
         />
       </div>
       <h1 className="text-center">Your shopping cart looks empty!</h1>
-      <div className="flex justify-center">
-        <Button
-          text="Go Shopping Now"
-          styling="bg-[#364968] p-3 rounded-[8px] w-[300px] text-white my-4"
-          onClick={() => router.push("/")}
-        />
-      </div>
     </div>
   );
 };
